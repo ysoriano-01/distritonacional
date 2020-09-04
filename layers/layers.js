0b9028195e4f5226293d294cd6aa812c06b44964
ysoriano-01 <70776591+ysoriano-01@users.noreply.github.com>
2020-09-04 15:59:29 -0400

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,127 @@
+var wms_layers = [];
+
+var format_Cuadrantez45KMLDEPTOC6_0 = new ol.format.GeoJSON();
+var features_Cuadrantez45KMLDEPTOC6_0 = format_Cuadrantez45KMLDEPTOC6_0.readFeatures(json_Cuadrantez45KMLDEPTOC6_0, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Cuadrantez45KMLDEPTOC6_0 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cuadrantez45KMLDEPTOC6_0.addFeatures(features_Cuadrantez45KMLDEPTOC6_0);
+var lyr_Cuadrantez45KMLDEPTOC6_0 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Cuadrantez45KMLDEPTOC6_0, 
+                style: style_Cuadrantez45KMLDEPTOC6_0,
+                interactive: true,
+                title: '<img src="styles/legend/Cuadrantez45KMLDEPTOC6_0.png" /> Cuadrantez-45KML DEPTO C-6'
+            });
+var format_Cuadrantez45KMLDEPTOC4_1 = new ol.format.GeoJSON();
+var features_Cuadrantez45KMLDEPTOC4_1 = format_Cuadrantez45KMLDEPTOC4_1.readFeatures(json_Cuadrantez45KMLDEPTOC4_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Cuadrantez45KMLDEPTOC4_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cuadrantez45KMLDEPTOC4_1.addFeatures(features_Cuadrantez45KMLDEPTOC4_1);
+var lyr_Cuadrantez45KMLDEPTOC4_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Cuadrantez45KMLDEPTOC4_1, 
+                style: style_Cuadrantez45KMLDEPTOC4_1,
+                interactive: true,
+                title: '<img src="styles/legend/Cuadrantez45KMLDEPTOC4_1.png" /> Cuadrantez-45KML DEPTO C-4'
+            });
+var format_Cuadrantez45KMLDEPTOC3_2 = new ol.format.GeoJSON();
+var features_Cuadrantez45KMLDEPTOC3_2 = format_Cuadrantez45KMLDEPTOC3_2.readFeatures(json_Cuadrantez45KMLDEPTOC3_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Cuadrantez45KMLDEPTOC3_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cuadrantez45KMLDEPTOC3_2.addFeatures(features_Cuadrantez45KMLDEPTOC3_2);
+var lyr_Cuadrantez45KMLDEPTOC3_2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Cuadrantez45KMLDEPTOC3_2, 
+                style: style_Cuadrantez45KMLDEPTOC3_2,
+                interactive: true,
+                title: '<img src="styles/legend/Cuadrantez45KMLDEPTOC3_2.png" /> Cuadrantez-45KML DEPTO C-3'
+            });
+var format_Cuadrantez45KMLDEPTOC2_3 = new ol.format.GeoJSON();
+var features_Cuadrantez45KMLDEPTOC2_3 = format_Cuadrantez45KMLDEPTOC2_3.readFeatures(json_Cuadrantez45KMLDEPTOC2_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Cuadrantez45KMLDEPTOC2_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cuadrantez45KMLDEPTOC2_3.addFeatures(features_Cuadrantez45KMLDEPTOC2_3);
+var lyr_Cuadrantez45KMLDEPTOC2_3 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Cuadrantez45KMLDEPTOC2_3, 
+                style: style_Cuadrantez45KMLDEPTOC2_3,
+                interactive: true,
+                title: '<img src="styles/legend/Cuadrantez45KMLDEPTOC2_3.png" /> Cuadrantez-45KML DEPTO C-2'
+            });
+var format_Cuadrantez45KMLDEPTOC1_4 = new ol.format.GeoJSON();
+var features_Cuadrantez45KMLDEPTOC1_4 = format_Cuadrantez45KMLDEPTOC1_4.readFeatures(json_Cuadrantez45KMLDEPTOC1_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Cuadrantez45KMLDEPTOC1_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cuadrantez45KMLDEPTOC1_4.addFeatures(features_Cuadrantez45KMLDEPTOC1_4);
+var lyr_Cuadrantez45KMLDEPTOC1_4 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Cuadrantez45KMLDEPTOC1_4, 
+                style: style_Cuadrantez45KMLDEPTOC1_4,
+                interactive: true,
+                title: '<img src="styles/legend/Cuadrantez45KMLDEPTOC1_4.png" /> Cuadrantez-45KML DEPTO C-1'
+            });
+var format_Cuadrantez45KML_Polygons__5 = new ol.format.GeoJSON();
+var features_Cuadrantez45KML_Polygons__5 = format_Cuadrantez45KML_Polygons__5.readFeatures(json_Cuadrantez45KML_Polygons__5, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Cuadrantez45KML_Polygons__5 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Cuadrantez45KML_Polygons__5.addFeatures(features_Cuadrantez45KML_Polygons__5);
+var lyr_Cuadrantez45KML_Polygons__5 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Cuadrantez45KML_Polygons__5, 
+                style: style_Cuadrantez45KML_Polygons__5,
+                interactive: true,
+                title: '<img src="styles/legend/Cuadrantez45KML_Polygons__5.png" /> Cuadrantez-45KML _Polygons_'
+            });
+var format_deptz451_Polygons__6 = new ol.format.GeoJSON();
+var features_deptz451_Polygons__6 = format_deptz451_Polygons__6.readFeatures(json_deptz451_Polygons__6, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_deptz451_Polygons__6 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_deptz451_Polygons__6.addFeatures(features_deptz451_Polygons__6);
+var lyr_deptz451_Polygons__6 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_deptz451_Polygons__6, 
+                style: style_deptz451_Polygons__6,
+                interactive: true,
+                title: '<img src="styles/legend/deptz451_Polygons__6.png" /> deptz-451 _Polygons_'
+            });
+
+lyr_Cuadrantez45KMLDEPTOC6_0.setVisible(true);lyr_Cuadrantez45KMLDEPTOC4_1.setVisible(true);lyr_Cuadrantez45KMLDEPTOC3_2.setVisible(true);lyr_Cuadrantez45KMLDEPTOC2_3.setVisible(true);lyr_Cuadrantez45KMLDEPTOC1_4.setVisible(true);lyr_Cuadrantez45KML_Polygons__5.setVisible(true);lyr_deptz451_Polygons__6.setVisible(true);
+var layersList = [lyr_Cuadrantez45KMLDEPTOC6_0,lyr_Cuadrantez45KMLDEPTOC4_1,lyr_Cuadrantez45KMLDEPTOC3_2,lyr_Cuadrantez45KMLDEPTOC2_3,lyr_Cuadrantez45KMLDEPTOC1_4,lyr_Cuadrantez45KML_Polygons__5,lyr_deptz451_Polygons__6];
+lyr_Cuadrantez45KMLDEPTOC6_0.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_Cuadrantez45KMLDEPTOC4_1.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_Cuadrantez45KMLDEPTOC3_2.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_Cuadrantez45KMLDEPTOC2_3.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_Cuadrantez45KMLDEPTOC1_4.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_Cuadrantez45KML_Polygons__5.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_deptz451_Polygons__6.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_Cuadrantez45KMLDEPTOC6_0.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_Cuadrantez45KMLDEPTOC4_1.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_Cuadrantez45KMLDEPTOC3_2.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_Cuadrantez45KMLDEPTOC2_3.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_Cuadrantez45KMLDEPTOC1_4.set('fieldImages', {'Name': 'TextEdit', 'description': 'TextEdit', 'timestamp': 'DateTime', 'begin': 'DateTime', 'end': 'DateTime', 'altitudeMode': 'TextEdit', 'tessellate': 'Range', 'extrude': 'Range', 'visibility': 'Range', 'drawOrder': 'Range', 'icon': 'TextEdit', });
+lyr_Cuadrantez45KML_Polygons__5.set('fieldImages', {'Name': '', 'description': '', 'timestamp': '', 'begin': '', 'end': '', 'altitudeMode': '', 'tessellate': '', 'extrude': '', 'visibility': '', 'drawOrder': '', 'icon': '', });
+lyr_deptz451_Polygons__6.set('fieldImages', {'Name': 'TextEdit', 'description': 'TextEdit', 'timestamp': 'DateTime', 'begin': 'DateTime', 'end': 'DateTime', 'altitudeMode': 'TextEdit', 'tessellate': 'Range', 'extrude': 'Range', 'visibility': 'Range', 'drawOrder': 'Range', 'icon': 'TextEdit', });
+lyr_Cuadrantez45KMLDEPTOC6_0.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_Cuadrantez45KMLDEPTOC4_1.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_Cuadrantez45KMLDEPTOC3_2.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_Cuadrantez45KMLDEPTOC2_3.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_Cuadrantez45KMLDEPTOC1_4.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_Cuadrantez45KML_Polygons__5.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_deptz451_Polygons__6.set('fieldLabels', {'Name': 'no label', 'description': 'no label', 'timestamp': 'no label', 'begin': 'no label', 'end': 'no label', 'altitudeMode': 'no label', 'tessellate': 'no label', 'extrude': 'no label', 'visibility': 'no label', 'drawOrder': 'no label', 'icon': 'no label', });
+lyr_deptz451_Polygons__6.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
